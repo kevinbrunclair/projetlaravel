b0eb6a75119885319801e34a0153fd6327940b59
Kévin BRUNCLAIRE <kevin.brunclaire@Digital-Grenoble.local>
2022-09-14 11:04:02 +0200

    ajout route pour chaque page

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('/', function() {
+    return 'Homepage';
+});
+
+Route::get('/product', function() {
+    return 'ListedesProduits';
+});
+
+Route::get('/product/{id}',function($id) {
+    return 'Ficheduproduit' . $id;
+});
+
+Route::get('/cart', function() {
+    return 'Panier';
 });
